986e8504d7fc4ba7c02380db5771be9bec06de73
andrewrgabb <39320782+andrewrgabb@users.noreply.github.com>
2020-08-21 18:39:22 +1000

    State moved up
    Successfully moved state from the board class up into the index class where it should be.

--- src/board.jsx
+++ src/board.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useMemo } from 'react';
+import React, { useMemo } from 'react';
 import styled from 'styled-components';
 import '@atlaskit/css-reset';
 import { DragDropContext, Droppable } from 'react-beautiful-dnd';
-import initialData from './initial-data';
+
 import Column from './column';
 
 const Container = styled.div `
@@ -19,92 +19,11 @@ function InnerList(props) {
   return <Column column={column} notes={notes} index={index} />;
 }
 
-export default function() {
-  const [state, setState] = useState(initialData);
-
-  let onDragEnd = result => {
-    const { destination, source, draggableId, type} = result;
-
-    if (!destination) {
-      return;
-    }
-
-    if (
-      destination.droppableId === source.droppableId &&
-      destination.index === source.index
-    ) {
-      return;
-    }
-
-    if (type === 'column') {
-      const newColumnOrder = Array.from(state.columnOrder);
-      newColumnOrder.splice(source.index, 1);
-      newColumnOrder.splice(destination.index, 0, draggableId);
-
-      const newState = {
-        ...state,
-        columnOrder: newColumnOrder,
-      };
-      setState(newState);
-      return;
-    }
-
-
-    const start = state.columns[source.droppableId];
-    const finish = state.columns[destination.droppableId];
-
-    // Moving notes within the same column
-    if (start === finish) {
-      const newNoteIds = Array.from(start.noteIds);
-      newNoteIds.splice(source.index, 1);
-      newNoteIds.splice(destination.index, 0, draggableId);
-
-      const newColumn = {
-        ...start,
-        noteIds: newNoteIds,
-      };
-
-      const newState = {
-        ...state,
-        columns: {
-          ...state.columns,
-          [newColumn.id]: newColumn,
-        },
-      };
-
-      setState(newState);
-      return;
-    }
-
-    // Moving notes from one column to another
-    const startNoteIds = Array.from(start.noteIds);
-    startNoteIds.splice(source.index, 1);
-    const newStart = {
-      ...start,
-      noteIds: startNoteIds,
-    };
-
-    const finishNoteIds = Array.from(finish.noteIds);
-    finishNoteIds.splice(destination.index, 0, draggableId);
-    const newFinish = {
-      ...finish,
-      noteIds: finishNoteIds,
-    };
-
-    const newState = {
-      ...state,
-      columns: {
-        ...state.columns,
-        [newStart.id]: newStart,
-        [newFinish.id]: newFinish,
-      },
-    };
-
-      setState(newState);
-  };
+export default function(props) {
+  
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
+    <DragDropContext onDragEnd={props.onDragEnd}>
       <Droppable
         droppableId='board' 
         direction='horizontal' 
@@ -115,10 +34,10 @@ export default function() {
             {...provided.droppableProps}
             ref={provided.innerRef} 
           >
-            {state.columnOrder.map((columnId, index) => {
-              const column = state.columns[columnId];
+            {props.columnOrder.map((columnId, index) => {
+              const column = props.columns[columnId];
               return (
-                <InnerList key={column.id} column={column} noteMap={state.notes} index={index} />
+                <InnerList key={column.id} column={column} noteMap={props.notes} index={index} />
               );
             })}
             {provided.placeholder}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
+import initialData from './initial-data';
 
 import Board from './board';
 
@@ -16,12 +17,96 @@ const Header = styled.h1`
 `;
 
 function App() {
+
+  const [state, setState] = useState(initialData);
+
+  let onDragEnd = result => {
+    const { destination, source, draggableId, type} = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    if (type === 'column') {
+      const newColumnOrder = Array.from(state.columnOrder);
+      newColumnOrder.splice(source.index, 1);
+      newColumnOrder.splice(destination.index, 0, draggableId);
+
+      const newState = {
+        ...state,
+        columnOrder: newColumnOrder,
+      };
+      setState(newState);
+      return;
+    }
+
+
+    const start = state.columns[source.droppableId];
+    const finish = state.columns[destination.droppableId];
+
+    // Moving notes within the same column
+    if (start === finish) {
+      const newNoteIds = Array.from(start.noteIds);
+      newNoteIds.splice(source.index, 1);
+      newNoteIds.splice(destination.index, 0, draggableId);
+
+      const newColumn = {
+        ...start,
+        noteIds: newNoteIds,
+      };
+
+      const newState = {
+        ...state,
+        columns: {
+          ...state.columns,
+          [newColumn.id]: newColumn,
+        },
+      };
+
+      setState(newState);
+      return;
+    }
+
+    // Moving notes from one column to another
+    const startNoteIds = Array.from(start.noteIds);
+    startNoteIds.splice(source.index, 1);
+    const newStart = {
+      ...start,
+      noteIds: startNoteIds,
+    };
+
+    const finishNoteIds = Array.from(finish.noteIds);
+    finishNoteIds.splice(destination.index, 0, draggableId);
+    const newFinish = {
+      ...finish,
+      noteIds: finishNoteIds,
+    };
+
+    const newState = {
+      ...state,
+      columns: {
+        ...state.columns,
+        [newStart.id]: newStart,
+        [newFinish.id]: newFinish,
+      },
+    };
+
+      setState(newState);
+  };
+
   return (
     <Structure>
       <Header id="Header">
         Light Notes
       </Header>
-      <Board />
+      <Board notes={state.notes} columns={state.columns} columnOrder={state.columnOrder} onDragEnd={onDragEnd}/>
     </Structure>
   );
 }
